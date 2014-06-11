b55206b8ec2041b98d6afee9137c3955f0ca79a2
Sebastian Weddmark Olsson <visnae@gmail.com>
2014-06-11 07:02:29 +0200

    added page layouts

--- app/server/router.js
+++ app/server/router.js
@@ -1,9 +1,10 @@
-var user = require( './controllers/view/user.js' ),
-    util = require( './controllers/view-utils.js' );
+var user  = require( './controllers/view/user.js' ),
+    index = require( './controllers/view/index.js' ),
+    util  = require( './controllers/view-utils.js' );
 
 module.exports = function( app ) {
-  app.get('/', user.attemptLogin);
-  app.post('/', user.login);
+  app.get('/',                               user.attemptLogin);
+  app.post('/',                              user.login);
   app.get('/home', util.ensureAuthenticated, index.home);
-  app.get('*', util.notfound);
+  app.get('*',                               util.notfound);
 };
